95f40090a422099559decb9e10f21fc00555dd9a
Elliot Morris <elliot.morris@foundry.com>
2024-08-25 16:16:22 +0100

    Sanitize inputs

--- SharedLogic/Utils/DataValidation.cs
+++ SharedLogic/Utils/DataValidation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MudBlazor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -58,17 +59,56 @@ namespace SharedLogic.Utils
 			return false;
 		}
 
+		//For use in mudblazor validation slots. Null means all good. Return value is the error that appears
+		public static string? SanitizeStringMudBlazorValidation(string text) {
+			try {
+				SanitizeString(text);
+			}
+			catch(Exception ex) {
+				return ex.Message;
+			}
+
+			return null;
+		}
+
+		public static string? SanitizeStringWithErrorSnackbar(string? text, ISnackbar snackBar) {
+			try {
+				return SanitizeString(text);
+			}
+			catch(Exception ex) {
+				snackBar.Add(ex.Message, Severity.Error);
+				return null;
+			}
+		}
+
         public static string SanitizeString(string text) {
-            if (text.Length < 3) {
-                throw new Exception($"{text} too short. Must be longer than 3 characters");
+			if (text == null) {
+                throw new Exception("Text is null");
             }
-            if (text.Length > 63) {
-                throw new Exception($"{text} too long. Must be shorter than 64 characters");
+            if (text.Length < 3) {
+                throw new Exception($"'{text}' is too short. Must be longer than 3 characters");
+            }
+            if (text.Length > 255) {
+                throw new Exception($"'{text}' is too long. Must be shorter than 256 characters");
             }
 
             return text;
         }
+
+		public static string? SanitizeMessageStringWithErrorSnackbar(string? message, ISnackbar snackBar) {
+			try {
+				return SanitizeMessageString(message);
+			}
+			catch(Exception ex) {
+				snackBar.Add(ex.Message, Severity.Error);
+				return null;
+			}
+		}
+
         public static string SanitizeMessageString(string text) {
+			if (text == null) {
+                throw new Exception("Message is null");
+            }
             if (text.Length > 1023) {
                 throw new Exception($"Message too long. Must be shorter than 1024 characters");
             }
