af28afb41bc692d204263efd06193d6eade0ad19
Elliot Morris <elliot.morris@foundry.com>
2024-10-06 03:00:45 +0100

    Add whitelist to CRUD

--- SharedLogic/Model/GameSettingsModel.cs
+++ SharedLogic/Model/GameSettingsModel.cs
@@ -13,5 +13,30 @@ namespace SharedLogic.Model
 		public bool showSceneTitle { get; set; } = true;
 		public bool playersCanMoveThemselvesBetweenScenes { get; set; } = false;
 		public SceneModel? defaultScene { get; set; }
+
+		public bool whitelistEnabled {get; set; } = false;
+
+
+		//So we can have reference mutability, for use with mudblazor datagrid
+		public class WhitelistName {
+			public WhitelistName() { name = ""; }
+			public WhitelistName(string? _name) { name = _name; }
+			public string? name = "";
+
+			public override int GetHashCode() => name?.GetHashCode() ?? 0;
+			public override string ToString() => name ?? "";
+
+            public override bool Equals(object? obj) {
+				// Check for null and type compatibility
+				if (obj == null || GetType() != obj.GetType())
+					return false;
+
+				// Cast the object to WhitelistName and compare the name
+				var other = (WhitelistName)obj;
+				return string.Equals(name, other.name, StringComparison.Ordinal);
+            }
+        }
+		public List<WhitelistName> whitelistNames { get; set; } = new List<WhitelistName>();
+
 	}
 }
