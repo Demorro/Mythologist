a25ea0409ec14471db57f8d409bc6174e57dfb55
Elliot Morris <elliot.morris@foundry.com>
2024-10-07 03:36:05 +0100

    Add "Send As" option to text pane

---
+++ SharedLogic/Utils/ReferenceString.cs
@@ -0,0 +1,28 @@
+﻿using SharedLogic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLogic.Utils
+{
+    public class ReferenceString
+    {
+        public ReferenceString(string _val) { value = _val; }
+        public string? value {get; set; }
+
+        // Overrides for Equals, GetHashCode and ToString are important for MudSelect
+		public override bool Equals(object o)
+		{
+			var other = o as ReferenceString;
+			return other?.value == value;
+		}
+		public override int GetHashCode() => value?.GetHashCode() ?? 0;
+
+		public override string ToString()
+		{
+			return value;
+		}
+    }
+}
